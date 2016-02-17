db3f9ed0cc54acfc945a4c167bd6f75255711b59
benvogcodes <benjamin.voglewede@gmail.com>
2016-02-17 08:46:19 -0800

    Generate favorite resource

--- gameconnect/config/routes.rb
+++ gameconnect/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :favorites
   resources :users
   resources :games
   # The priority is based upon order of creation: first created -> highest priority.
