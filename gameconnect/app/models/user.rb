6050d43b89ec576ddabf497d471bd9fc10f2c814
benvogcodes <benjamin.voglewede@gmail.com>
2016-02-17 09:10:47 -0800

    Add user associations and validations, correct field password_hash to password_digest

--- gameconnect/app/models/user.rb
+++ gameconnect/app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :favorites, dependent: :destroy
+  has_many :games, through: :favorites, dependent: :destroy
+
+  validates :username, prescence: true
+  validates :username, uniqueness: true
+
+  validates :email, prescence: true
+  validates :email, uniqueness: true
+
+  validates :password, prescence: true
+
 end
